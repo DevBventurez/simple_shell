33fc80a15ba0634051919ec15ef4e41266ca186e
DevBventurez <bonveturew@gmail.com>
2023-12-13 08:35:55 -0800

    simple_shell

--- _atoi.c
+++ _atoi.c
@@ -1,14 +1,15 @@
-nclude "shell.h"
+#include "shell.h"
 
 /**
  * interactive - returns true if shell is interactive mode
- * @infor: struct address
+ * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <=2);}
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
 
 /**
  * is_delim - checks if character is a delimeter
@@ -25,9 +26,9 @@ int is_delim(char c, char *delim)
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
 
 int _isalpha(int c)
@@ -39,16 +40,17 @@ int _isalpha(int c)
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
